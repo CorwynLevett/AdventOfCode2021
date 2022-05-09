ad942e11501d04f43070c3d3cf08044d831420c5
Corwyn Levett <corwyn.levett@towerswatson.com>
2022-05-09 15:03:58 +0100

    git ignore again

---
+++ AOC Day 1 - 2021/AOC Day 1 - 2021/HelpfulTools.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC___2021
+{
+    public class HelpfulTools
+    {
+        public static string WhatsTheMostCommon(string[] input, int startPosition, int stopPosition)
+        {
+            int zeroCount = 0;
+            int oneCount = 0;
+            string mostCommmonBit = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if (input[i].Substring(startPosition, stopPosition) == "0")
+                {
+                    zeroCount++;
+                }
+                else if (input[i].Substring(startPosition, stopPosition) == "1")
+                {
+                    oneCount++;
+                }
+            }
+            if (zeroCount > oneCount)
+            {
+                mostCommmonBit = "0";
+            }
+            else
+            {
+                mostCommmonBit = "1";
+            }
+
+            return mostCommmonBit;
+        }
+
+        public static string InvertBinary(string binary)
+        {
+            char[] arr = binary.ToCharArray(); 
+            string invert = "";
+            foreach (char digit in arr)
+            {
+                if (digit.Equals('1'))
+                {
+                    invert += "0";
+                }
+                else if (digit.Equals('0'))
+                {
+                    invert += "1";
+                }
+            }
+            return invert;
+        }
+    }
+}
